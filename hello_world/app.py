4eeafca405cf4234a2e57c1aaeaa17548657781a
jorgetovar <jorgetovar621@gmail.com>
2023-06-07 14:20:19 -0500

    Enable CircleCI integration

--- hello_world/app.py
+++ hello_world/app.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world - We are going to create a DevOps as a service powered by IA and Copilot",
+            "message": "We are going to create a DevOps as a service powered by IA and Copilot",
             "random_info": random_info
 
         }),

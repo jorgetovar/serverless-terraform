fc12c664cae9ea6c6119ffd4bee7bfa07ee81c13
jorgetovar <jorgetovar621@gmail.com>
2024-03-27 12:18:41 -0500

    GH Copilot

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,11 @@
 terraform {
 
+  # Set the following environment variables, which Terraform will access in your build environment to configure both the AWS provider and the Terraform Cloud integration for your project:
+  # Set AWS_ACCESS_KEY_I
+  # Set AWS_SECRET_ACCESS_KEY
+  # Set TF_CLOUD_ORGANIZATION
+  # Set TF_WORKSPACE
+  # Set TF_TOKEN_app_terraform_io
   cloud {
     organization = "jorgetovar"
 
